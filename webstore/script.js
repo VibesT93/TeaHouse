335895cb82ba29c4655aa992cfa2364b04ad2ed4
vinnyWebDev <vinnywebdev@gmail.com>
2023-04-07 14:49:05 +0100

    updated webstroe

--- webstore/script.js
+++ webstore/script.js
@@ -1,14 +1,17 @@
+//hides tea form the products page
 function hideTea() {
     document.getElementById("teaGrid").style.display = "none";
     document.getElementById("accessoriesGrid").style.display="flex";
 
 }
 
+//hides accessories from the propducts page
 function hideAccessories() {
     document.getElementById("accessoriesGrid").style.display = "none";
     document.getElementById("teaGrid").style.display="flex";
 }
 
+//displays all products on the product page
 function hideNone(){
     document.getElementById("accessoriesGrid").style.display = "flex";
     document.getElementById("teaGrid").style.display="flex";
