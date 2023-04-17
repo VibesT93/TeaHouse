7f890471ae61fd02bac209106b09286695806eda
Violet Thomson <violetthomson93@gmail.com>
2023-04-17 18:53:41 +0100

    Fizing css

--- webstore/script.js
+++ webstore/script.js
@@ -27,16 +27,16 @@ function teaQuiz(){
     document.getElementById("quiz-title").style.display= "none";
 
     if (z.checked == true){
-        document.getElementById("newText").innerHTML = "We would suggest a highly oxidised tea. Such as an Irish Black Tea or Ceylon Black";
+        document.getElementsByClassName"newText").innerHTML = "We would suggest a highly oxidised tea. Such as an Irish Black Tea or Ceylon Black";
     }
     else if (x.checked == true && y.checked == true){
-        document.getElementById("newText").innerHTML = "We would suggest a tea made with flowers and herbs. Such as an Hibiscus Tea or Apple Fruit Tea";
+        document.getElementsByClassName("newText").innerHTML = "We would suggest a tea made with flowers and herbs. Such as an Hibiscus Tea or Apple Fruit Tea";
     }
     else if (x.checked == false && y.checked == true){
-        document.getElementById("newText").innerHTML = "We would suggest a tea made with a strong herbal taste. Such as an Nettle Tea or Wellness Tea";
+        document.getElementsByClassName("newText").innerHTML = "We would suggest a tea made with a strong herbal taste. Such as an Nettle Tea or Wellness Tea";
     }
     else {
-        document.getElementById("newText").innerHTML = "We would suggest a natural processed tea. Such as our Oolong tea";
+        document.getElementsByClassName("newText").innerHTML = "We would suggest a natural processed tea. Such as our Oolong tea";
     }
     
 }
